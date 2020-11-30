0a6076871fa51c069d690b40a2c4de072d4eb0a3
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-11-30 12:20:28 +0700

    Create jsostream(Lib).ts

---
+++ typescript/jsostream(Lib).ts
@@ -0,0 +1,21 @@
+/*******************************************************\
+ *              COPYRIGHT 2020 - 2025                  *
+ *******************************************************
+ *   Have privacy police this made by website          *
+ *   https://jonathanmarpaung.rf.gd/                   *
+ *   this for publish CODE                             *
+ *((((((((((((((((((((((((((C))))))))))))))))))))))))))*
+\*******************************************************/
+
+declare function require(name: string);
+declare let exports: any;
+
+const process = require('process');
+
+exports.jsostream = function(callback) {
+    try {
+        callback(process.argv.length, process.argv);
+    } catch (err) {
+        console.log(err);
+    }
+}
