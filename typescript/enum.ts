a7b2c6b88f14443085f75b062e3a687d11977427
jonathanmarp <60574415+jonathanmarp@users.noreply.github.com>
2020-11-30 12:47:44 +0700

    Update enum.ts

--- typescript/enum.ts
+++ typescript/enum.ts
@@ -18,12 +18,12 @@ class enums {
             this._0x02.push(_0x02 + 1);
         }
     }
-    changeFirstNumber(_0x01) {
+    changeFirstNumber(_0x01: any) {
         for (let _0x03 = 0; _0x03 < this._0x01.length; _0x03++) {
             this._0x02[_0x03] = _0x03 + _0x01;
         }
     }
-    getEnum(_0x01) {
+    getEnum(_0x01: any) {
         for (let _0x02 = 0; _0x02 < this._0x01.length; _0x02++) {
             if (this._0x01[_0x02] == _0x01) {
                 return this._0x02[_0x02];
